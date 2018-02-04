c1d8aa14ab06cef30d9f09eae4af87a18c0f5c74
Aamir Sayid <aamir.sayid@gmail.com>
2018-02-04 15:25:21 +0530

    Adds full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
-  def full_title
+  def full_title(page_title)
+    base_title = "Prawo"
+
+    if page_title.blank?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
   end
 end
