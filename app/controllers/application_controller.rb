ad01a47c664f4f8be73bd200a49b6ec5ad987932
Aamir Sayid <aamir.sayid@gmail.com>
2018-02-21 20:18:06 +0530

    Adds flag controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,32 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+      def check_admin
+        if user_signed_in?
+          if current_user.admin?
+            # No problem. Go ahead.
+          else
+            flash[:alert] = 'Only admins can access this page.'
+            redirect_to root_url
+          end
+        else
+          flash[:alert] = 'You need to sign in to access this page.'
+          redirect_to root_url
+        end
+      end
+
+      def check_enabled
+        if user_signed_in?
+          if current_user.enabled?
+            # No problem. Go ahead.
+          else
+            flash[:alert] = 'Your account has been disabled. Please contact portal admin.'
+            redirect_to root_url
+          end
+        else
+          flash[:alert] = 'You need to sign in to access this page.'
+          redirect_to root_url
+        end
+      end
 end
